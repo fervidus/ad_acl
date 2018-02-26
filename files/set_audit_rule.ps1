f42255ca97ac378ce04538d0fd3dd3bac5d7975a
Bryan Belanger <bryan@autostructure.io>
2018-02-25 23:06:23 -0500

    Add powershell scripts to do audit and access operations

---
+++ files/set_audit_rule.ps1
@@ -0,0 +1,53 @@
+param(
+
+      [Parameter(Position=0,Mandatory=$true)]
+      [System.String]$SecurityIdentifier,
+
+      [Parameter(Position=1,Mandatory=$true)]
+      [ValidateSet('Failure','None','Success')]
+      [System.String]$AuditFlags,
+
+      [Parameter(Position=2,Mandatory=$true)]
+      [ValidateSet('All','Children','Descendents','None','SelfAndChildren')]
+      [System.String]$ActiveDirectorySecurityInheritance,
+
+      [Parameter(Position=3,Mandatory=$true)]
+      [ValidateSet('CreateChild','DeleteChild','ListChildren','Self','ReadProperty','WriteProperty','DeleteTree','ListObject','ExtendedRight','Delete','ReadControl','GenericExecute','GenericWrite','GenericRead','WriteDacl','WriteOwner','GenericAll','Synchronize','AccessSystemSecurity')]
+      [System.String[]]$ActiveDirectoryRights,
+
+      [Parameter(Position=4,Mandatory=$true)]
+      [System.String]$Path,
+
+      [Parameter(Position=5,Mandatory=$false)]
+      [System.String]$ObjectGuid='00000000-0000-0000-0000-000000000000',
+
+      [Parameter(Position=6,Mandatory=$false)]
+      [System.String]$InheritedObjectGuid='00000000-0000-0000-0000-000000000000'
+  )
+Import-Module ActiveDirectory
+
+$my_acl = Get-Acl -Path "Microsoft.ActiveDirectory.Management\ActiveDirectory:://RootDSE/$Path"
+
+$ActiveDirectoryRightsArray = @()
+
+foreach ($i in $ActiveDirectoryRights) {
+$ActiveDirectoryRightsArray += [System.DirectoryServices.ActiveDirectoryRights]::$i
+}
+
+$objUser = New-Object System.Security.Principal.NTAccount($SecurityIdentifier)
+
+$objSid = $objUser.Translate([System.Security.Principal.SecurityIdentifier])
+
+$objectGuidObj = New-Object System.Guid($ObjectGuid)
+$inheritedObjectGuidObj = New-Object System.Guid($InheritedObjectGuid)
+
+$AuditRule = New-Object System.DirectoryServices.ActiveDirectoryAuditRule($objSid,
+  $ActiveDirectoryRightsArray,
+  [System.Security.AccessControl.AuditFlags]::$AuditFlags,
+  $objectGuidObj,
+  [System.DirectoryServices.ActiveDirectorySecurityInheritance]::$ActiveDirectorySecurityInheritance,
+  $inheritedObjectGuidObj)
+
+$my_acl.AddAuditRule($AuditRule)
+
+Set-Acl -Path "Microsoft.ActiveDirectory.Management\ActiveDirectory:://RootDSE/$Path" -AclObject $my_acl
