10f529e0ce5827c7968f353d06538540123a60b4
Jack Coleman <jack@autostructure.io>
2018-04-09 17:44:30 -0400

    Confine fact to DCs only

--- lib/facter/root_domain.rb
+++ lib/facter/root_domain.rb
@@ -4,13 +4,21 @@ Facter.add(:root_domain) do
   setcode do
     require 'win32/registry'
 
+    # Check if the machine is a Domain Controller
     value = nil
-
-    hive = Win32::Registry::HKEY_LOCAL_MACHINE
-    hive.open('SYSTEM\CurrentControlSet\Services\NTDS\Parameters', Win32::Registry::KEY_READ) do |reg|
-      value = reg['Root Domain']
+    val = nil
+    type = Win32::Registry::HKEY_LOCAL_MACHINE
+    type.open('SYSTEM\CurrentControlSet\Control\ProductOptions', Win32::Registry::KEY_READ) do |r|
+      val = r['ProductType']
     end
 
+    # If the machine is a DC, it is safe to open this key without errors
+    if val == 'LanmanNT'
+      hive = Win32::Registry::HKEY_LOCAL_MACHINE
+      hive.open('SYSTEM\CurrentControlSet\Services\NTDS\Parameters', Win32::Registry::KEY_READ) do |reg|
+        value = reg['Root Domain']
+      end
+    end
     value
   end
 end
