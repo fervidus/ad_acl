ef70d487a6964c4c16726f4747665676548b52e9
Jack Coleman <jack@autostructure.io>
2018-04-11 15:21:58 -0400

    Change regex to allow for domain SIDs

--- lib/puppet/type/ad_acl.rb
+++ lib/puppet/type/ad_acl.rb
@@ -78,7 +78,7 @@ Puppet::Type.newtype(:ad_acl) do
     desc 'Access rules associated with this acl'
 
     validate do |value|
-      unless /^S-\d-\d+-(\d+-){1,14}\d+$/.match(value['identity'])
+      unless /^S-\d+.*$/.match(value['identity'])
         raise ArgumentError,
           "Access rules currently only accept SIDs as identifiers"
       else
